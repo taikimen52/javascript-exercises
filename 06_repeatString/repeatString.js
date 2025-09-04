01c51bd99a3d83b3f14c926dc653cb7d74d61d1c
Taikimen52 <5taikiiida@gmail.com>
2025-09-04 11:50:38 +0900

    Done exercise06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ""
+    for(let i = 0; i < num; i++){
+        result += string; 
+    }
+    if(num < 0){
+        return "ERROR"
+    }else{
+        return result;
+    }
 };
+
+repeatString('hey', 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("googbye", -1);
+
+const number = Math.floor(Math.random() * 1000);
+repeatString("hey", number);
+repeatString(" ", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
