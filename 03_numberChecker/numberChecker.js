249b2b93d2844938280169cfe00143090e148157
Taikimen52 <5taikiiida@gmail.com>
2025-09-04 11:11:59 +0900

    Done exercise03

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,18 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000) {
+    return true;
+  } else if(number === 10){
     return true;
   } else {
     return false;
   }
 }
+
+numberChecker(1000);
+numberChecker(10);
+numberChecker(9);
+numberChecker(6);
+
 
 // Do not edit below this line
 module.exports = numberChecker;
