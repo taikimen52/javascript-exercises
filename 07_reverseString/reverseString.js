cd631cddd589cc97e775d52018b9c304857aaadc
Taikimen52 <5taikiiida@gmail.com>
2025-09-04 12:05:36 +0900

    Done exercise07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length
+    let reverseStr = ""
+    for(let i = 1; i <= length; i++){
+        reverseStr += string.charAt(length-i)
+    }
+    return reverseStr;
 };
 
+reverseString("hello");
+reverseString("hello there");
+reverseString('123! abc! Hello, Odinite.');
+reverseString(" ");
 // Do not edit below this line
 module.exports = reverseString;
