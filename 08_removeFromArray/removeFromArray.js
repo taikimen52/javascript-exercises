e62ed1715ad12e020144fbd0403d051004ed6793
Taikimen52 <5taikiiida@gmail.com>
2025-09-09 15:33:36 +0900

    Done 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    return arr.filter((item) => !targets.includes(item))
 };
+
+
+
+
+removeFromArray([1, 2, 3, 4], 3)
+removeFromArray([1, 2, 3, 4], 3, 2)
+removeFromArray([1, 2, 2, 3], 2)
+removeFromArray([1, 2, 3, 4], 7, "tacos")
+removeFromArray([1, 2, 3, 4], 7, 2)
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
