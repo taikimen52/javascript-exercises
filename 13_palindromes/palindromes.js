a2be8e8f10ed2f80ea9278e639ee79fbcc624734
Taikimen52 <5taikiiida@gmail.com>
2025-09-18 11:30:12 +0900

    Done : exrcise13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const processed = text.toLowerCase().replace(/[^a-z0-9]/g, "")
+    const reversed = processed.split("").reverse().join("");
+    return processed === reversed;
 
 };
+
+palindromes('racecar')
+palindromes('racecar!')
+palindromes('A car, a man, a maraca.')
+palindromes('Animal loots foliated detail of stool lamina.')
+palindromes('ZZZZ car, a man, a maracaz.')
+palindromes('rac3e3car')
+palindromes('r3ace3car')
 
 // Do not edit below this line
 module.exports = palindromes;
