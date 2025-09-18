007c5ff69b02c6bd059dd7f7a6499c3c762510ca
Taikimen52 <5taikiiida@gmail.com>
2025-09-18 11:59:13 +0900

    Fix : exercise15 solution

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,23 +1,26 @@
 const books = [
-      {
-        title: 'Book',
-        author: 'Name'
-      },
-      {
-        title: 'Book2',
-        author: 'Name2'
-      }
-    ]
-
-const getTheTitles = function(arr) {
-    const titleArr =[];
-    for(let i = 0; i < arr.length; i++){
-        const target = arr[i].title;
-        titleArr.push(target);
+    {
+    title: 'Book',
+    author: 'Name'
+    },
+    {
+    title: 'Book2',
+    author: 'Name2'
     }
-    return titleArr;
-     
-};
+]
+
+const getTheTitles = (arr) =>{
+    return arr.map(item => item.title);
+}
+
+// const getTheTitles = function(arr) {
+//     const titleArr =[];
+//     for(let i = 0; i < arr.length; i++){
+//         const target = arr[i].title;
+//         titleArr.push(target);
+//     }
+//     return titleArr;     
+// };
 
 getTheTitles(books)
 
