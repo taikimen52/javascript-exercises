9686799c5e353cfb857ed669a47bff7f82e56852
Taikimen52 <5taikiiida@gmail.com>
2025-09-18 13:16:10 +0900

    Done : exrcise16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1066,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
 
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
+
+  return arr.reduce((oldest, person) => {
+    const age = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+    return age > oldestAge ? person : oldest; 
+  });
 };
+findTheOldest(people);
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
