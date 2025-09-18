cca884f5a26beb1eeaaf5cf10d0e80d53774efab
Taikimen52 <5taikiiida@gmail.com>
2025-09-18 11:56:02 +0900

    Done : exercise15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
 
+const getTheTitles = function(arr) {
+    const titleArr =[];
+    for(let i = 0; i < arr.length; i++){
+        const target = arr[i].title;
+        titleArr.push(target);
+    }
+    return titleArr;
+     
 };
+
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
